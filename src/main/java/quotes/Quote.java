acd264cf393c60b1982a9c704270afcfb89d4bfb
Dayne Daylong <dalexad@gmail.com>
2020-01-17 13:44:13 -0800

    completed MOST not ALL of feature task

--- src/main/java/quotes/Quote.java
+++ src/main/java/quotes/Quote.java
@@ -3,10 +3,45 @@ package quotes;
 public class Quote {
      String text;
      String author;
+     String starWarsQuote;
+     int id;
 
-    public Quote(String text, String author) {
-        this.text = text;
-        this.author = author;
-    }
+//    public QuoteFromAuthor(String text, String author) {
+//        this.text = text;
+//        this.author = author;
+//    }
+
+//    public QuoteFromStarWars(int id, String starWarsQuote) {
+//        this.id = id;
+//        this.starWarsQuote = starWarsQuote;
+//    }
+
 }
+
+
+
+//public class Cat {
+//    String name;
+//    boolean sharpClaws;
+//    String color;
+//    String favoriteFood;
+//    int hairLengthInCm;
+//
+//    public Cat(String name, boolean sharpClaws, String color, String favoriteFood, int hairLengthInCm) {
+//        this.name = name;
+//        this.sharpClaws = sharpClaws;
+//        this.color = color;
+//        this.favoriteFood = favoriteFood;
+//        this.hairLengthInCm = hairLengthInCm;
+//    }
+//
+//    public void sneak(){
+//        if(this.sharpClaws == false){
+//            System.out.println("sneak sneak sneak sneak");
+//        } else {
+//            System.out.println("click click click click");
+//        }
+//    }
+//}
+
 
--- src/test/java/quotes/AppTest.java
+++ src/test/java/quotes/AppTest.java
@@ -1,9 +1,13 @@
 package quotes;
 
+
 import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
+
 
 public class AppTest {
     @Test public void testIfReturningAQuoteAndAuthor() {
-        
+        String expected = App.getAuthorQuote();
+        assertNotNull(expected);
     }
 }
